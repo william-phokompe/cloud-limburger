114ed72bbef45f385fb64f2762eb70433d2d848d
William Phokompe <phokompe.william@outlook.com>
2019-09-13 16:21:44 +0200

    Add PropTypes & Style to JSX
    Using inline styling, through a method

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+
+    getStyle = () => {
+        return {
+            textDecoration: this.props.task.completed ? 'line-through' : 'none',
+            backgroundColor: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted'
+        }
+    };
+
+    markComplete = (e) => {
+        console.log(this.props);
+    };
+
     render() {
         return (
-            <div>
-                
+            <div style={ this.getStyle() }>
+                <input type="checkbox" onChange={ this.props.markComplete } /> {' '} 
+              { this.props.task.title }
             </div>
         )
     }
 }
+
+// Validation for properties that a component should have
+// Property types
+TodoItem.protoType = {
+    todo: PropTypes.object.isRequired
+};
 
 export default TodoItem
