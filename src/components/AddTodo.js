d6fe838e3051135054d1e6a1f74114e53400c6e5
William Phokompe <phokompe.william@outlook.com>
2019-09-16 16:51:31 +0200

    Add a button to add a task

---
+++ src/components/AddTodo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class AddTodo extends Component {
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text" 
+                    name="title" 
+                    placeholder="Add Todo..."
+                />
+                <input type="submit" value="Submit" className="btn"/>
+            </form>
+        )
+    }
+}
+
+export default AddTodo ;
