44953343faa13c886c7ced494aec81f0f57e3724
William Phokompe <phokompe.william@outlook.com>
2019-09-17 14:36:40 +0200

    For routing changes, add an about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        // <React.Fragment>
+            <div>
+                <h1>About</h1>
+            <p>
+                This is a Todo list app v1.0.0.
+                Part of a React crash course.
+            </p>
+            </div>
+        // {/* </React.Fragment> */}
+    )
+}
